62923082ed9c0208f11e1df43d5a6c57f3c83962
Carlos Lins <carloslinsalbertoa@gmail.com>
2022-07-23 19:27:22 -0300

    Componentes de classes com estado (stateful)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react'
 
-function App() {
+
+class App extends Component {
+    state={
+      name: 'carlos',
+      counter: 0
+    }
+
+  handlePClick = () => { 
+    this.setState({name: "carlin"})
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault()
+    const {counter} = this.state
+    this.setState({counter: counter + 1})
+  }
+
+  render(){
+    const {name, counter} = this.state
+
+    return(
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={this.handlePClick}>
+          {name} {counter}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          
+        </a>
+      </header>
+    </div>
+  )}
+}
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,5 +61,5 @@ function App() {
     </div>
   );
 }
-
+ */
 export default App;
